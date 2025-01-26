84da33c16649d8a2993aa305fcc1d0a6dc9767a5
Masianuy <Masianuy@ukr.net>
2025-01-26 13:31:59 +0200

    added file deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   base: "/netflix/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
