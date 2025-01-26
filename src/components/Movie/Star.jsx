2c5af7bb75f09960b75edf4fb47bf543120370a7
Masianuy <Masianuy@ukr.net>
2025-01-26 12:06:05 +0200

    added hover

--- src/components/Movie/Star.jsx
+++ src/components/Movie/Star.jsx
@@ -11,6 +11,7 @@ const Star = (props) => {
       <path
         d="M15 0L10.45 9.22L0.280029 10.69L7.64003 17.87L5.90003 28L15 23.22L24.1 28L22.36 17.87L29.72 10.7L19.55 9.22L15 0Z"
         stroke='#fff'
+        filter='drop-shadow(0 0 3px #FFEA2B)'
       />
     </svg>
   );
